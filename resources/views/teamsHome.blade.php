06bac5f21bb416b7be99c94995ed57306b6a06d7
JoanCarlesG <juancarlos.godinezm@gmail.com>
2023-02-06 11:35:15 +0100

    Working delete teams

--- resources/views/teamsHome.blade.php
+++ resources/views/teamsHome.blade.php
@@ -31,8 +31,13 @@
                 <td class="px-6 py-4">{{ $team->home_city }}</td>
                 <td class="px-6 py-4">{{ $team->stadium }}</td>
                 <td class="px-6 py-4">
+                    <form action="{{ route('teams.destroy', $team->team_id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
                     <a href="teams/{{ $team->team_id }}/edit" class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-2">Edit</a>
-                    <button class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
+
+                    <button onclick="return confirmDelete()" type="submit" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
+                    </form>
                 </td>
             </tr>
             @endforeach
@@ -40,3 +45,13 @@
     </table>
 </div>
 @endsection
+
+<script>
+function confirmDelete() {
+    var result = confirm("Are you sure you want to delete this team?");
+    if (!result) {
+        return false 
+    }
+}
+
+</script>
