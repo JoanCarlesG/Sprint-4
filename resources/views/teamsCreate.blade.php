9eeb6b3bd95cb53d5cdfc9d9ff0b2432f8d0bbf1
JoanCarlesG <juancarlos.godinezm@gmail.com>
2023-02-07 10:45:08 +0100

    cancel buttons fixed

--- resources/views/teamsCreate.blade.php
+++ resources/views/teamsCreate.blade.php
@@ -78,6 +78,9 @@
                         </div>
                     </div>
                     <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                        <button class="inline-flex justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+                            <a href="/teams">Cancel</a>
+                        </button>
                         <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
                     </div>
                 </div>
